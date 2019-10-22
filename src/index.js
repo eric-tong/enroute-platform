800222223041ba300e21004e518cfb9103d8e9ab
eric-tong <erictong.designs@gmail.com>
2019-10-22 16:15:18 +0100

    ENR-2 Implement Postgres

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 // @flow
 
 import express from "express";
-import storage from "node-persist";
+import { Pool } from "pg";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const key = "locations";
+const pool = new Pool({
+  user: "eric",
+  host: "localhost",
+  database: "eric",
+  password: "",
+  port: 5432,
+});
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.get("/locations/:action", async (req, res) => {
-  await storage.init({});
-  switch (req.params.action) {
-    case "clear":
-      await storage.clear();
-      break;
-    case "new":
-      const locations = (await storage.getItem(key)) || [];
-      console.log(req.query[key]);
-      const newLocations = JSON.parse(req.query[key]).payload;
-      await storage.updateItem(key, [...locations, ...newLocations]);
-      break;
-  }
-  res.send(await storage.getItem(key));
+app.get("/", async (req, res) => {
+  pool.query("SELECT * FROM users WHERE id = $1", [1], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    if (results) {
+      res.status(200).json(results.rows);
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
