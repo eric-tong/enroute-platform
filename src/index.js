5a31ef8edbd29a1de0e08f8db9ce8c854580fb17
eric-tong <erictong.designs@gmail.com>
2019-10-22 10:48:31 +0100

    ENR-0 Add dynamic port listening

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/location", (req, res) => res.send(JSON.stringify(req.query)));
