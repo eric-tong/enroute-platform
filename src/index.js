f7c829d6ed59908af609446e414fc78100fe760b
eric-tong <erictong.designs@gmail.com>
2019-10-22 12:01:29 +0100

    ENR-1 Add node persist

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/location", (req, res) => res.send(JSON.stringify(req.query)));
+app.get("/location", async (req, res) => {
+  await storage.init({});
+
+  Object.entries(req.query).forEach(async ([key, value]) => {
+    await storage.setItem(key, value);
+    console.log(key, value);
+  });
+  return res.send(await storage.values());
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
