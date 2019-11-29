7b213221421579b5e5e16a2e640d546362712431
eric-tong <erictong.designs@gmail.com>
2019-11-29 17:00:08 +0000

    [Database] Adapt to camel case database headers

--- src/data/busStops.js
+++ src/data/busStops.js
@@ -11,11 +11,11 @@ export type BusStop = {|
   icon: string,
 |};
 
-const GET_ALL_BUS_STOPS = "SELECT * FROM busStops";
+const GET_ALL_BUS_STOPS = `SELECT * FROM "busStops"`;
 const GET_ALL_BUS_STOPS_IN_ORDER = `
-SELECT coords FROM busStops INNER JOIN arrivals 
-  ON busStops.id = arrivals.busStopId 
-  WHERE arrivals.tripId = 1
+SELECT coords FROM "busStops" INNER JOIN arrivals 
+  ON "busStops".id = arrivals."busStopId"
+  WHERE arrivals."tripId" = 1
   ORDER BY time
 `;
 
