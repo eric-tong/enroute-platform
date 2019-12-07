83a3b6e673c20f06b683b8a9b32080c7fc4091d2
eric-tong <erictong.designs@gmail.com>
2019-12-07 15:37:20 +0000

    [Codec8] Parse first three parts

--- src/trackers/__test/codec8.test.js
+++ src/trackers/__test/codec8.test.js
@@ -1,48 +1,25 @@
 // @flow
 
 import packet from "./packetData";
+import parse from "../codec8Parser";
 
-/*
+const stream =
+  "000000000000003608010000016B40D8EA30010000000000000000000000000000000105021503010101425E0F01F10000601A014E0000000000000000010000C7CF";
+const data = parse(stream);
 
-Bytes   Name                Description
+describe("Codec8 parsing", () => {
+  test("parses preamble", () => {
+    const expected = 0;
+    expect(data.preamble).toBe(expected);
+  });
 
-4       Preamble            0x00000000
-4       Data field length   Size from Codec Id to Number of Data 2
-1       Codec ID            For Codec8 it's 0x08
-1       Number of Data 1    A number which defines how many records is in the packet
-X       AVL Data            Array (See below)
-1       Number of Data 2    Same as Number of Data 1
-4       CRC-16              Calculated from Codec ID to the Number of Data 2 (CRC-16/IBM)
+  test("parses data field length", () => {
+    const expected = parseInt("00000036", 16);
+    expect(data.dataFieldLength).toBe(expected);
+  });
 
-(AVL DATA)
-8       Timestamp           Milliseconds between the current time and midnight, January, 1970 UTC (UNIX time).
-1       Priority            0 - Low, 1 - High, 2 - Panic
-4       Longitude           East–west position
-4       Latitude            North–south position
-2       Altitude            Meters above sea level
-2       Angle               Degrees from north pole
-1       Satellites          Number of visible satellites
-2       Speed               If GPS data is invalid, speed will be 0x0000
-
-1       Event IO ID         If data is acquired on event – this field defines which IO property has changed and generated an event.
-                            For example, when if Ignition state changed and it generate event, Event IO ID will be 
-                            0xEF (AVL ID: 239). If it’s not eventual record – the value is 0.
-1       Total IO Count      N1 + N2 + N3 + N4
-1       N1                  Number of 1-byte IO items
-2*N1    ID/Value            ID - 1 byte, value - 1 byte
-1       N2                  Number of 2-byte IO items
-2*N2    ID/Value            ID - 1 byte, value - 2 byte
-1       N4                  Number of 1-byte IO items
-2*N4    ID/Value            ID - 1 byte, value - 4 byte
-1       N8                  Number of 1-byte IO items
-2*N8    ID/Value            ID - 1 byte, value - 8 byte
-
-(IO Data)
-
-*/
-
-describe("Codec8 Parsing", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(Array.isArray(packet)).toBe(true);
+  test("parses codec id", () => {
+    const expected = 8;
+    expect(data.codecId).toBe(8);
   });
 });
---
+++ src/trackers/codec8Parser.js
@@ -0,0 +1,62 @@
+// @flow
+
+/*
+
+Bytes   Name                Description
+
+4       Preamble            0x00000000
+4       Data field length   Size from Codec Id to Number of Data 2
+1       Codec ID            For Codec8 it's 0x08
+1       Number of Data 1    A number which defines how many records is in the packet
+X       AVL Data            Array (See below)
+1       Number of Data 2    Same as Number of Data 1
+4       CRC-16              Calculated from Codec ID to the Number of Data 2 (CRC-16/IBM)
+
+(AVL DATA)
+8       Timestamp           Milliseconds between the current time and midnight, January, 1970 UTC (UNIX time).
+1       Priority            0 - Low, 1 - High, 2 - Panic
+4       Longitude           East–west position
+4       Latitude            North–south position
+2       Altitude            Meters above sea level
+2       Angle               Degrees from north pole
+1       Satellites          Number of visible satellites
+2       Speed               If GPS data is invalid, speed will be 0x0000
+
+1       Event IO ID         If data is acquired on event – this field defines which IO property has changed and generated an event.
+                            For example, when if Ignition state changed and it generate event, Event IO ID will be 
+                            0xEF (AVL ID: 239). If it’s not eventual record – the value is 0.
+1       Total IO Count      N1 + N2 + N3 + N4
+1       N1                  Number of 1-byte IO items
+2*N1    ID/Value            ID - 1 byte, value - 1 byte
+1       N2                  Number of 2-byte IO items
+2*N2    ID/Value            ID - 1 byte, value - 2 byte
+1       N4                  Number of 1-byte IO items
+2*N4    ID/Value            ID - 1 byte, value - 4 byte
+1       N8                  Number of 1-byte IO items
+2*N8    ID/Value            ID - 1 byte, value - 8 byte
+
+Items that need verifying: Zero bytes, data field length, CRC
+
+*/
+
+type DataPart = $Exact<{ start: number, length: number }>;
+
+const preamble: DataPart = { start: 0, length: 4 };
+const dataFieldLength: DataPart = { start: 4, length: 4 };
+const codecId: DataPart = { start: 8, length: 1 };
+
+export default function parse(stream: string) {
+  const getValue = ({ start, length }: DataPart) =>
+    parseInt(stream.substr(start * 2, length * 2), 16);
+
+  const parts: { [string]: DataPart } = {
+    preamble,
+    dataFieldLength,
+    codecId,
+  };
+
+  return Object.keys(parts).reduce<{ [string]: number }>(
+    (total, key) => Object.assign({}, total, { [key]: getValue(parts[key]) }),
+    {}
+  );
+}
