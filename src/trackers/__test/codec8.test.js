56074be42494739cf241802d058339cd52d80417
eric-tong <erictong.designs@gmail.com>
2019-12-07 10:44:26 +0000

    [Codec 8] Add test file

---
+++ src/trackers/__test/codec8.test.js
@@ -0,0 +1,9 @@
+// @flow
+
+import packet from "./packetData";
+
+describe("Codec8 Parsing", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(Array.isArray(packet)).toBe(true);
+  });
+});
