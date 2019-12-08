378e957b11c0f75640f6157b6abc1a11a20cca5e
eric-tong <erictong.designs@gmail.com>
2019-12-08 17:56:13 +0000

    [TCP] Refactor

--- src/trackers/tcpServer.js
+++ src/trackers/tcpServer.js
@@ -32,32 +32,37 @@ const server = net.createServer((socket: Socket) => {
       stream: stream.length < 100 ? stream.toString() : stream.toString("hex"),
     });
 
-    if (!client.imei) {
-      let ip, imei;
-
-      if (!client.ip) {
-        [ip, imei] = stream.toString().split("\n");
-        client.ip = ip;
-        if (!imei) return;
-        else imei = imei.slice(2);
-      }
-
-      if (!imei) imei = stream.slice(2).toString();
-      if (validImeis.includes(imei)) {
-        client.imei = imei;
-        socket.write(IMEI_REPLY.ACCEPT);
-      } else {
-        socket.write(IMEI_REPLY.REJECT);
-        console.log(`Invalid IMEI ${imei}`);
-      }
+    if (!client.ip) {
+      const [ip, imei] = stream.toString().split("\n");
+      client.ip = ip;
+      if (!imei) return;
+      else setImei(imei);
+    } else if (!client.imei) {
+      setImei(stream);
     } else {
       const data: any = parseCodec8Stream(stream.toString("hex"));
       console.log(data);
-      socket.write(Buffer.from([(0, 0, 0, data.avlDataCount)]));
+      write(Buffer.from([(0, 0, 0, data.avlDataCount)]));
     }
   });
 
   socket.on("end", () => console.log(`Disconnected from ${client.name}`));
+
+  function setImei(stream: string | Buffer) {
+    const imei = stream.slice(2).toString();
+    if (validImeis.includes(imei)) {
+      client.imei = imei;
+      write(IMEI_REPLY.ACCEPT);
+    } else {
+      write(IMEI_REPLY.REJECT);
+      console.log(`Invalid IMEI ${imei}`);
+    }
+  }
+
+  function write(message: string | Buffer) {
+    console.log("Write", message);
+    socket.write(message);
+  }
 });
 
 server.listen(port, () =>
