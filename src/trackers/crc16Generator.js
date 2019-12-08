9854ff940bfc3ffb997696a6a17154a3fb2d6629
eric-tong <erictong.designs@gmail.com>
2019-12-08 22:51:02 +0000

    [CRC] Fix crc shifting bug

--- src/trackers/crc16Generator.js
+++ src/trackers/crc16Generator.js
@@ -2,16 +2,13 @@
 
 export default function generateCRC16(stream: Buffer) {
   let crc = 0;
-  stream
-    .reverse()
-    .slice(0)
-    .forEach(byte => {
-      crc ^= byte;
-      for (let i = 0; i < 8; i++) {
-        const carry = crc & 1;
-        crc >> 1;
-        if (carry == 1) crc ^= 0xa001;
-      }
-    });
+  stream.forEach(byte => {
+    crc ^= byte;
+    for (let i = 0; i < 8; i++) {
+      const carry = crc & 1;
+      crc >>>= 1;
+      if (carry == 1) crc ^= 0xa001;
+    }
+  });
   return crc;
 }
