7dd06c8b835485a042962c7eab46a7a99d584584
eric-tong <erictong.designs@gmail.com>
2020-01-12 11:06:30 +0800

    [Predictor] Add check for bus angle during bus stop visit

--- src/predictor/busStopVisits.js
+++ src/predictor/busStopVisits.js
@@ -5,13 +5,18 @@ import database from "../database/database";
 const GEOFENCE_RADIUS = 0.001;
 const SAVE_BUS_STOP_VISIT_FROM_AVL_ID = `
 WITH avl AS (
-    SELECT id, longitude, latitude FROM avl WHERE id = $1
+    SELECT id, longitude, latitude, angle FROM avl WHERE id = $1
 )
 
 INSERT INTO bus_stop_visits
 SELECT avl.id as avl_id, bus_stops.id as bus_stop_id
     FROM bus_stops CROSS JOIN avl
     WHERE CIRCLE(POINT(bus_stops.longitude, bus_stops.latitude), ${GEOFENCE_RADIUS}) @> POINT(avl.longitude, avl.latitude)
+    AND (
+        bus_stops.road_angle = NULL
+        OR ABS(bus_stops.road_angle - avl.angle) < 45
+        OR ABS(bus_stops.road_angle - avl.angle) > 315
+    )
     RETURNING *
 `;
 const GET_AVL_ID_IN_DATE =
