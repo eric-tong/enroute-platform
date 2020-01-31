b745b52230b7b1985117a705725f7ebd2eadf2f6
eric-tong <erictong.designs@gmail.com>
2020-01-31 11:57:30 +0000

    [Test] Add check in returns undefined with invalid department test

--- src/resolvers/__test/CheckInResolver.test.js
+++ src/resolvers/__test/CheckInResolver.test.js
@@ -21,25 +21,38 @@ import { createCheckIn } from "../CheckInResolver";
 import database from "../../database/database";
 
 describe("check in resolver", () => {
-  test("creates new check in", async () => {
-    const vehicle = await insertVehicle({ registration: "XXXXXXX" });
-    const department = await insertDepartment({
-      id: 200,
-      type: "newDepartment"
+  describe("creates new check in", () => {
+    test("with valid department", async () => {
+      const vehicle = await insertVehicle({ registration: "XXXXXXX" });
+      const department = await insertDepartment({
+        id: 200,
+        type: "newDepartment"
+      });
+
+      const actual = await createCheckIn(undefined, {
+        departmentType: department.type,
+        vehicleRegistration: vehicle.registration
+      });
+      const expected = {
+        id: expect.anything(),
+        timestamp: expect.anything(),
+        departmentId: department.id,
+        vehicleId: vehicle.id
+      };
+
+      expect(actual).toEqual(expected);
     });
 
-    const actual = await createCheckIn(undefined, {
-      departmentType: department.type,
-      vehicleRegistration: vehicle.registration
-    });
-    const expected: CheckIn = {
-      id: expect.anything(),
-      timestamp: expect.anything(),
-      departmentId: department.id,
-      vehicleId: vehicle.id
-    };
+    test("returns undefined with invalid department", async () => {
+      const vehicle = await insertVehicle({ registration: "XXXXXXX" });
 
-    expect(actual).toEqual(expected);
+      const actual = await createCheckIn(undefined, {
+        departmentType: "invalidDepartment",
+        vehicleRegistration: vehicle.registration
+      });
+
+      expect(actual).toBeUndefined();
+    });
   });
 
   beforeAll(clearTables);
