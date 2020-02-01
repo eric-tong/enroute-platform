efe351c1238d94b7fc2001fb632d11856654e349
eric-tong <erictong.designs@gmail.com>
2020-02-01 18:50:59 +0000

    [Departure] Remove get relevant departures

--- src/resolvers/DepartureResolver.js
+++ src/resolvers/DepartureResolver.js
@@ -37,39 +37,3 @@ export async function getDeparturesFromTripId(
       }))
   );
 }
-
-function getRelevantDepartures(
-  predictedDepartures: BusArrival[],
-  scheduledDepartures: ScheduledDeparture[],
-  maxLength: number,
-  upcomingOnly: boolean = true
-) {
-  const now = DateTime.local();
-  const relevantDepartures: Departure[] = [];
-  for (const scheduledDeparture of scheduledDepartures) {
-    if (relevantDepartures.length >= maxLength) break;
-    const scheduledTime = toActualTime(scheduledDeparture.minuteOfDay);
-    const predictedArrival = predictedDepartures.find(
-      predictedDeparture =>
-        predictedDeparture.tripId === scheduledDeparture.tripId &&
-        predictedDeparture.busStopId === scheduledDeparture.busStopId
-    );
-    if (
-      predictedArrival ||
-      !upcomingOnly ||
-      scheduledTime.valueOf() + DEPARTURE_BUFFER >= now.valueOf()
-    ) {
-      relevantDepartures.push({
-        scheduled: scheduledTime.toSQL(),
-        predicted: (predictedArrival
-          ? predictedArrival.dateTime
-          : scheduledTime
-        ).toSQL(),
-        tripId: scheduledDeparture.tripId,
-        busStopId: scheduledDeparture.busStopId
-      });
-    }
-  }
-
-  return relevantDepartures;
-}
