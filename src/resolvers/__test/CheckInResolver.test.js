89fc75e359788692b8eb90fadf749ee2c1b974e5
eric-tong <erictong.designs@gmail.com>
2020-02-23 14:17:50 +0000

    [Check In] Fix check out test

--- src/resolvers/__test/CheckInResolver.test.js
+++ src/resolvers/__test/CheckInResolver.test.js
@@ -7,46 +7,42 @@ import database from "../../database/database";
 import { insertBusStop } from "../../__test/insert";
 
 describe("check in resolver", () => {
-  describe("creates new check in", () => {
-    test.only("with valid bus stop", async () => {
+  test("creates new check in", async () => {
+    const userId = randomId();
+    const origin = await insertBusStop({});
+    const destination = await insertBusStop({});
+    const remarks = "new remark";
+
+    const actual = await createCheckIn(undefined, {
+      userId,
+      originId: origin.id,
+      destinationId: destination.id,
+      remarks
+    });
+    const expected = {
+      id: expect.anything(),
+      timestamp: expect.anything(),
+      userId,
+      originId: origin.id,
+      destinationId: destination.id,
+      remarks
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  describe("checks out", () => {
+    test("with valid check in id", async () => {
       const userId = randomId();
       const origin = await insertBusStop({});
       const destination = await insertBusStop({});
       const remarks = "new remark";
 
-      const actual = await createCheckIn(undefined, {
-        userId,
-        originId: origin.id,
-        destinationId: destination.id,
-        remarks
-      });
-      const expected = {
-        id: expect.anything(),
-        timestamp: expect.anything(),
-        userId,
-        originId: origin.id,
-        destinationId: destination.id,
-        remarks
-      };
-
-      expect(actual).toEqual(expected);
-    });
-
-    test("returns undefined with invalid department", async () => {
-      const actual = await createCheckIn(undefined, {
-        departmentType: "invalidDepartment",
-        vehicleRegistration: vehicle.registration
-      });
-
-      expect(actual).toBeUndefined();
-    });
-  });
-
-  describe("checks out", () => {
-    test("with valid check in id", async () => {
       const checkIn = await createCheckIn(undefined, {
-        departmentType: department.type,
-        vehicleRegistration: vehicle.registration
+        userId,
+        originId: origin.id,
+        destinationId: destination.id,
+        remarks
       });
 
       const actual = await checkOutFromCheckInId(undefined, { id: checkIn.id });
