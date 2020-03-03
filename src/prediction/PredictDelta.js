7df74ab5da7de2129603fa5f9073d5a49dc76a28
eric-tong <erictong.designs@gmail.com>
2020-03-03 11:27:57 +0000

    [Prediction] Add predictDelta method

---
+++ src/prediction/PredictDelta.js
@@ -0,0 +1,27 @@
+// @flow
+
+import "../service/config";
+const tf = require("@tensorflow/tfjs");
+
+const MAX_DISTANCE = 10000;
+const MAX_DELTA = 600;
+
+export default async function predictDelta(tripId: number, distance: number) {
+  const model = await getModel(tripId).catch(error => {
+    console.error(error);
+    return null;
+  });
+  if (!model) return 0;
+
+  const prediction = await model.predict(
+    tf.tensor1d([distance / MAX_DISTANCE])
+  );
+  const delta = prediction.dataSync()[0] * MAX_DELTA;
+  return delta;
+}
+
+async function getModel(tripId: number) {
+  return await tf.loadLayersModel(
+    `file://${process.env.MODELS_PATH ?? ""}/trip-${tripId}/model.json`
+  );
+}
--- src/service/config.js
+++ src/service/config.js
@@ -1,8 +1,14 @@
 // @flow
 
 import { Settings } from "luxon";
+import { nodeFileSystemRouter } from "../prediction/NodeFileSystem";
 
 Settings.defaultLocale = "en-GB";
 Settings.defaultZoneName = "Europe/London";
 
 require("dotenv").config();
+
+// Register the model saving and loading handlers for the 'file://' URL scheme.
+const tf = require("@tensorflow/tfjs");
+tf.io.registerLoadRouter(nodeFileSystemRouter);
+tf.io.registerSaveRouter(nodeFileSystemRouter);
